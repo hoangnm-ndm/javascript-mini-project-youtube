58cca2a64aa6e3d30366a4c8f0696d0e4b1f7ef4
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-20 08:49:42 +0700

    add signin/signup

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,38 @@
+const SignUpPage = () => {
+  return /*html*/ `
+    <form action="">
+      <h1>Đăng ký tài khoản mới!</h1>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="password" />
+      </div>
+
+      <div class="mb-3">
+        <label for="confirmPass" class="form-label">Confirm password</label>
+        <input type="password" class="form-control" id="confirmPass" />
+      </div>
+
+      <button
+        type="button"
+        id="btnRegister"
+        onclick="register()"
+        class="btn btn-primary w-100"
+      >
+        Đăng ký
+      </button>
+
+      <p>Đã có tài khoản? Ấn <a href="/login">Đăng nhập</a>ngay!</p>
+    </form>
+  `;
+};
+
+export default SignUpPage;
--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,6 +1,27 @@
 const Dashboard = () => {
   return `
-  
+  <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Image</th>
+      <th scope="col">Description</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
   `;
 };
 
