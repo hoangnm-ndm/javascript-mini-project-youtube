19e37023a313d133633a23322eb332383c79fd8c
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-24 18:16:13 +0700

    test deploy

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -8,28 +8,28 @@ const ProductDetail = (data) => {
       .get(`/products/${id}`)
       .then(({ data }) => {
         productDetail.innerHTML = /*html*/ `
-      <div class="row">
-        <div class="col-6">
-          <img src="${data.thumbnail}" alt="${data.title}" />
-        </div>
-        <div class="col-6">
-          <div class="product-infor">
-            <h1>${data.title}</h1>
-            <div>Giá: ${data.price}</div>
-            <p>Sẵn hàng: ${data.stock}</p>
-            <p>Đánh giá: ${data.rating}</p>
-            <p>Mô tả: ${data.description}</p>
-            <a href="/" class="btn btn-danger">Thêm vào giỏ hàng</a>
+          <div class="row">
+            <div class="col-6">
+              <img src="${data.thumbnail}" alt="${data.title}" />
+            </div>
+            <div class="col-6">
+              <div class="product-infor">
+                <h1>${data.title}</h1>
+                <div>Giá: ${data.price}</div>
+                <p>Sẵn hàng: ${data.stock}</p>
+                <p>Đánh giá: ${data.rating}</p>
+                <p>Mô tả: ${data.description}</p>
+                <a href="/" class="btn btn-danger">Thêm vào giỏ hàng</a>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      `;
+        `;
       })
       .catch(() => {
         productDetail.innerHTML = /*html*/ `
-      <h2>Sản phẩm không tồn tại</h2>
-      <a href="/">Quay lại trang chủ</a>
-      `;
+          <h2>Sản phẩm không tồn tại</h2>
+          <a href="/">Quay lại trang chủ</a>
+        `;
       });
   }
 };
